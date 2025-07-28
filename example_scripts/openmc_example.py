f63cd6eb9d9fba63bd206ad9c1936f4beed6bdd1
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-07-28 11:46:34 -0400

    addedfestim model

--- example_scripts/openmc_example.py
+++ example_scripts/openmc_example.py
@@ -1,72 +1,155 @@
 import openmc
 import openmc_data_downloader as odd
-import matplotlib.pyplot as plt
-
-# Materials
-
-lithium = openmc.Material(name="lithium")
-lithium.set_density("g/cc", 0.534)
-lithium.add_element("Li", 1.0)
-
-mats = openmc.Materials([lithium])
-
-odd.download_cross_section_data(
-    mats,
-    libraries=["FENDL-3.1d"],
-    set_OPENMC_CROSS_SECTIONS=True,
-    particles=["neutron"],
-)
-
-# Geometry
+from openmc2dolfinx import StructuredGridReader
+import festim as F
+import numpy as np
+import dolfinx
+from mpi4py import MPI
 
 dim = 60
-cube_surface = openmc.model.RectangularParallelepiped(-dim, dim, -dim, dim, -dim, dim)
-region = -cube_surface
-cell = openmc.Cell(region=region, fill=lithium)
-
-vacuum_surf = openmc.Sphere(r=dim * 2, boundary_type="vacuum")
-vacuum_region = +cube_surface & -vacuum_surf
-vacuum = openmc.Cell(region=vacuum_region, fill=None)
 
 
-geometry = openmc.Geometry([cell, vacuum])
+def run_openmc_example():
+    # Materials
+
+    lithium = openmc.Material(name="lithium")
+    lithium.set_density("g/cc", 0.534)
+    lithium.add_element("Li", 1.0)
+
+    mats = openmc.Materials([lithium])
+
+    odd.download_cross_section_data(
+        mats,
+        libraries=["FENDL-3.1d"],
+        set_OPENMC_CROSS_SECTIONS=True,
+        particles=["neutron"],
+    )
+
+    # Geometry
+
+    cube_surface = openmc.model.RectangularParallelepiped(
+        -dim, dim, -dim, dim, -dim, dim
+    )
+    region = -cube_surface
+    cell = openmc.Cell(region=region, fill=lithium)
+
+    vacuum_surf = openmc.Sphere(r=dim * 2, boundary_type="vacuum")
+    vacuum_region = +cube_surface & -vacuum_surf
+    vacuum = openmc.Cell(region=vacuum_region, fill=None)
+
+    geometry = openmc.Geometry([cell, vacuum])
+
+    # Tallies
+    tally = openmc.Tally(name="tritium_production")
+    tally.scores = ["(n,Xt)"]
+    mesh = openmc.RegularMesh()
+    mesh.dimension = [30, 30, 15]
+    mesh.lower_left = [-dim, -dim, -dim]
+    mesh.upper_right = [dim, dim, dim]
+    tally.filters = [openmc.MeshFilter(mesh)]
+
+    # cell_filter = openmc.CellFilter(cell)
+    # tally.filters = [cell_filter]
+
+    tallies = openmc.Tallies([tally])
+
+    # Settings
+
+    source = openmc.IndependentSource()
+    source_pos_z = dim + 10
+    source.space = openmc.stats.Point((0, 0, source_pos_z))
+    source.angle = openmc.stats.Isotropic()
+    source.energy = openmc.stats.Discrete([14.1e6], [1.0])
+
+    settings = openmc.Settings()
+    settings.run_mode = "fixed source"
+    settings.source = source
+    settings.batches = 10
+    settings.particles = 10000
+
+    my_model = openmc.Model(
+        geometry=geometry, settings=settings, materials=mats, tallies=tallies
+    )
+
+    my_model.run(apply_tally_results=True)
+
+    mesh.write_data_to_vtk(
+        "tritium_production_mesh.vtk", {"tritium_production": tally.mean}
+    )
 
 
-# Tallies
-tally = openmc.Tally(name="tritium_production")
-tally.scores = ["(n,Xt)"]
-mesh = openmc.RegularMesh()
-mesh.dimension = [30, 30, 15]
-mesh.lower_left = [-dim, -dim, -dim]
-mesh.upper_right = [dim, dim, dim]
-tally.filters = [openmc.MeshFilter(mesh)]
+def festim_model(use_openmc_mesh=True):
+    """
+    Args:
+        use_openmc_mesh (bool, optional): if True, FESTIM will use the OpenMC mesh, otherwise
+            we make our own.
+    """
+    # read openmc vtk
+    reader = StructuredGridReader("tritium_production_mesh.vtk")
+    reader.create_dolfinx_mesh()
 
-# cell_filter = openmc.CellFilter(cell)
-# tally.filters = [cell_filter]
+    my_model = F.HydrogenTransportProblem()
 
-tallies = openmc.Tallies([tally])
+    tritium = F.Species("T")
 
-# Settings
+    if use_openmc_mesh:
+        my_model.mesh = F.Mesh(reader.dolfinx_mesh)
+    else:
+        refined_mesh = dolfinx.mesh.create_box(
+            MPI.COMM_WORLD,
+            points=[(-dim, -dim, -dim), (dim, dim, dim)],
+            n=(30, 30, 70),
+            cell_type=dolfinx.mesh.CellType.tetrahedron,
+        )
+        my_model.mesh = F.Mesh(refined_mesh)
 
-source = openmc.IndependentSource()
-source_pos_z = dim + 10
-source.space = openmc.stats.Point((0, 0, source_pos_z))
-source.angle = openmc.stats.Isotropic()
-source.energy = openmc.stats.Discrete([14.1e6], [1.0])
+    boundary = F.SurfaceSubdomain(id=1)
 
-settings = openmc.Settings()
-settings.run_mode = "fixed source"
-settings.source = source
-settings.batches = 10
-settings.particles = 10000
+    my_mat = F.Material(D_0=1, E_D=0)
 
-my_model = openmc.Model(
-    geometry=geometry, settings=settings, materials=mats, tallies=tallies
-)
+    volume = F.VolumeSubdomain(id=1, material=my_mat)
 
-my_model.run(apply_tally_results=True)
+    my_model.subdomains = [boundary, volume]
+
+    my_model.boundary_conditions = [
+        F.FixedConcentrationBC(subdomain=boundary, value=0.0, species=tritium)
+    ]
+
+    if use_openmc_mesh:
+        source_term = reader.create_dolfinx_function("tritium_production")
+    else:
+        original_source_term = reader.create_dolfinx_function("tritium_production")
+        V = dolfinx.fem.functionspace(refined_mesh, ("DG", 0))
+        source_term = dolfinx.fem.Function(V)
+        F.helpers.nmm_interpolate(source_term, original_source_term)
+        # export to VTX
+        writer = dolfinx.io.VTXWriter(MPI.COMM_WORLD, "source.bp", [source_term], "BP5")
+        writer.write(0.0)
+
+    my_model.sources = [
+        F.ParticleSource(
+            value=source_term,
+            volume=volume,
+            species=tritium,
+        )
+    ]
+
+    my_model.species = [tritium]
+
+    my_model.temperature = 300
+
+    my_model.settings = F.Settings(
+        atol=1e-10,
+        rtol=1e-10,
+        transient=False,
+    )
+
+    my_model.exports = [F.VTXSpeciesExport(filename="tritium_conc.bp", field=tritium)]
+
+    my_model.initialise()
+    my_model.run()
 
 
-mesh.write_data_to_vtk(
-    "tritium_production_mesh.vtk", {"tritium_production": tally.mean}
-)
+if __name__ == "__main__":
+    run_openmc_example()
+    festim_model(use_openmc_mesh=False)
